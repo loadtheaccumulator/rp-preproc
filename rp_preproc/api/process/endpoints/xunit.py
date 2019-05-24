f93572599c7652f2538a1d22d6b8fedb3ed52d2c
Jonathan Holloway <jholloway@redhat.com>
2019-05-24 10:37:56 -0500

    testing...
    Signed-off-by: Jonathan Holloway <jholloway@redhat.com>

--- rp_preproc/api/process/endpoints/xunit.py
+++ rp_preproc/api/process/endpoints/xunit.py
@@ -11,7 +11,7 @@ from rp_preproc.api.restplus import api
 
 log = logging.getLogger(__name__)
 
-ns = api.namespace('process/xunit',
+ns = api.namespace('xunit',
                    description='Operations related to importing xunit XML')
 
 
