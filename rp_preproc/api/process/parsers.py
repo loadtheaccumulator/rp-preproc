83546d319f5aad69b0866d743448e45c46de220c
Jonathan Holloway <jholloway@redhat.com>
2019-05-28 20:12:35 -0500

    add help for swagger UI
    Signed-off-by: Jonathan Holloway <jholloway@redhat.com>

--- rp_preproc/api/process/parsers.py
+++ rp_preproc/api/process/parsers.py
@@ -5,13 +5,20 @@ from rp_preproc.api.restplus import api
 
 
 upload_parser = api.parser()
-upload_parser.add_argument('endpoint', location='form', required=True)
-upload_parser.add_argument('api_token', location='form', required=True)
-upload_parser.add_argument('project', location='form', required=True)
+upload_parser.add_argument('endpoint', location='form', required=True,
+                           help='The path to the ReportPortal API')
+upload_parser.add_argument('api_token', location='form', required=True,
+                           help='The user\'s API token from profile')
+upload_parser.add_argument('project', location='form', required=True,
+                           help='The ReportPortal project')
 upload_parser.add_argument('file', location='files',
-                           type=FileStorage, required=True)
+                           type=FileStorage, required=True,
+                           help='An xUnit XML result file')
 
 example_parser = api.parser()
-example_parser.add_argument('endpoint', location='form', required=True)
-example_parser.add_argument('api_token', location='form', required=True)
-example_parser.add_argument('project', location='form', required=True)
+example_parser.add_argument('endpoint', location='form', required=True,
+                            help='The path to the ReportPortal API')
+example_parser.add_argument('api_token', location='form', required=True,
+                            help='The user\'s API token from profile')
+example_parser.add_argument('project', location='form', required=True,
+                            help='The ReportPortal project')
